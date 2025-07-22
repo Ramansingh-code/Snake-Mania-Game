430aa0c50f9cb22897219f52f5819a92255a62ee
Ramansingh-code <ramansingh8957@gmail.com>
2025-07-23 01:03:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('requirment/food.mp3');
-const gameOverSound = new Audio('requirment/gameover.mp3');
-const moveSound = new Audio('requirment/move.mp3');
-const musicSound = new Audio('requirmentmusic.mp3');
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
 let speed = 9;
 let score = 0;
 let lastPaintTime = 0;
